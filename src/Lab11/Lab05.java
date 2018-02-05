ed249ccc2b9ab5b583552709bfe13beee5007b7f
Elena <Hellen_87.87@mail.ru>
2018-02-05 20:46:17 +0600

    February 5th

---
+++ src/Lab11/Lab05.java
@@ -0,0 +1,17 @@
+package Lab11;
+
+public class Lab05 {
+
+    public static Boolean task8878(int x, int b){
+
+        return x < b;
+
+
+
+    }
+    if
+
+    public static Boolean task8715(int a, int b, int c){
+            return (a<b && b<c);
+    }
+}
